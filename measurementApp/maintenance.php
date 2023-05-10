e168b32815f403d7c4ee2c5451ad43ec1472eec0
aniq321 <fnaticniq@gmail.com>
2023-05-10 10:25:46 +0800

    save

---
+++ measurementApp/maintenance.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convert Mass</title>
+</head>
+<body>
+    <div id="main-content">
+        <h1 style="text-align: center;">WEBSITE IN MAINTENANCE MODE !!!</h1>
+        <img src="" alt="">
+        <p style="text-align: center;">Back To <a href="index.php">Main Menu</a></p>
+    </div>
+</body>
+</html>
